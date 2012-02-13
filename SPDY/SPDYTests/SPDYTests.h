9d4eacae152a7ef3ba26f08093d19f6b3e30e2a5
Jim Morrison <jim@twist.com>
2012-02-13 13:27:26 -0800

    Update company name.

--- SPDY/SPDYTests/SPDYTests.h
+++ SPDY/SPDYTests/SPDYTests.h
@@ -3,7 +3,7 @@
 //  SPDYTests
 //
 //  Created by Jim Morrison on 2/9/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Twist Inc. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
